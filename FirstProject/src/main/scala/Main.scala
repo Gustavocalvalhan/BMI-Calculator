eb301ade2ef5e52583f8fcc8f6544be844ebee22
Gustavo Calvalhan <gustavocalvalhan@Air-de-Gustavo.lan>
2022-07-11 10:59:40 +0300

    new version

--- FirstProject/src/main/scala/Main.scala
+++ FirstProject/src/main/scala/Main.scala
@@ -4,38 +4,69 @@ import scala.math.BigDecimal.double2bigDecimal
 
 object Main extends App {
 
-  var a: Double = 0
-  var p: Double = 0
+  var a: String = ""
+  var p: String = ""
   var imc: Double = 0
+  var aIsDigit: Boolean = false
+  var pIsDigit: Boolean = false
 
-  println("please inform your height: ")
-  a = readInt()
+  try {
 
-  println("Please inform you weight: ")
-  p = readInt()
+    while (aIsDigit == false) {
 
-  imc = (p / ((a * a) / 10000)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble;
+      println("please inform your height: ")
+      a = readLine()
 
-  if (imc < 17) {
-    println("You are very underweight, your BMI is: " + (imc))
-  }
+      if (a.forall(_.isDigit)) {
+        aIsDigit = true
+      } else {
+        println("please inform your height again with just numbers: ")
+      }
+    }
+
+
+    while (pIsDigit == false) {
+
+      println("please inform your weight: ")
+      p = readLine()
+
+      if (p.forall(_.isDigit)) {
+        pIsDigit = true
+      } else {
+        println("please inform your weight again with just numbers: ")
+
+      }
+    }
+
+    imc = (p.toDouble / ((a.toDouble * a.toDouble) / 10000)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble;
+
+    if (imc < 17) {
+      println("You are very underweight, your BMI is: " + (imc))
+    }
     if (imc > 17 && imc <= 18.99) {
-      println("Underweight, your BMI is: "+ (imc))
-  }
-      if (imc > 18.5 && imc <= 24.99) {
-        println("Congratulations!! you are at your ideal weight your BMI is: "+ (imc))
-  }
-        if (imc > 25 && imc <= 29.99) {
-          println("Overweight, your BMI is: " + (imc))
-  }
-          if (imc > 30 && imc <= 34.99) {
-            println("Obesity, your BMI is: "+ (imc))
-  }
-            if (imc > 35 && imc <= 40) {
-              println("Severe Obesity, your BMI and: "+ (imc))
+      println("Underweight, your BMI is: " + (imc))
     }
-              if (imc > 40) {
-                println("Morbid Obesity: "+ (imc))
+    if (imc > 18.5 && imc <= 24.99) {
+      println("Congratulations!! you are at your ideal weight your BMI is: " + (imc))
+    }
+    if (imc > 25 && imc <= 29.99) {
+      println("Overweight, your BMI is: " + (imc))
+    }
+    if (imc > 30 && imc <= 34.99) {
+      println("Obesity, your BMI is: " + (imc))
+    }
+    if (imc > 35 && imc <= 40) {
+      println("Severe Obesity, your BMI and: " + (imc))
+    }
+    else {
+      println("Morbid Obesity: " + (imc))
     }
 
+
   }
+
+  catch {
+    case _: Throwable => println("Just inform number")
+  }
+}
+
